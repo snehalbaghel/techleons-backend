0db6680244f9b2f723bb4799292ed583e8f17974
Snehal Baghel <snehalbaghel@gmail.com>
2019-02-12 10:58:38 +0530

    changes admin schema to hold only one event

--- models/admin.js
+++ models/admin.js
@@ -7,7 +7,7 @@ const Schema = mongoose.Schema;
 const AdminSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
-    events: {type: [Schema.Types.ObjectId]},
+    event: {type: Schema.Types.ObjectId, ref: 'Event', required: true},
 })
 
 AdminSchema.methods.setPassword = function(pass) {
